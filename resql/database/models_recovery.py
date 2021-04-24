6e0dee503fca4cb98aef55db8a28a63790b56ec6
Gabriel Galli <ggabriel96@hotmail.com>
2021-04-23 23:09:24 -0300

    Added QueryLog.executed_at with server_default of func.now()

--- resql/database/models_recovery.py
+++ resql/database/models_recovery.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, JSON, String, Text
+from sqlalchemy import Column, DateTime, Integer, JSON, String, Text, func
 from sqlalchemy.orm import registry
 
 mapper_registry = registry()
@@ -10,6 +10,7 @@ class QueryLog:
     # add some engine identifier (e.g. pymysql, psycopg2, etc)
 
     id = Column(Integer, primary_key=True)
+    executed_at = Column(DateTime, nullable=False, server_default=func.now())
     parameters = Column(JSON, nullable=True)
     statement = Column(Text, nullable=False)
     type = Column(String(32), nullable=False)
