05b8af091075ddefb5e52ac1954dca156dfdcab1
Gabriel Galli <ggabriel96@hotmail.com>
2021-04-24 12:53:31 -0300

    Little improvement to initial test

--- tests/auditing/test_insert.py
+++ tests/auditing/test_insert.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from sqlalchemy import select
 from sqlalchemy.future import Engine
 from sqlalchemy.orm import sessionmaker
@@ -13,7 +15,12 @@ def test_orm_insert_should_be_audited(
     production_mksession: sessionmaker,
 ) -> None:
     # Arrange
+    now = dt.datetime.utcnow()
     person = Person(name="Someone", age=25)
+    expected_diff = dict(
+        name=dict(old=None, new="Someone"),
+        age=dict(old=None, new=25),
+    )
 
     # Act
     log_changes(of=production_mksession, to=audit_engine)
@@ -26,3 +33,7 @@ def test_orm_insert_should_be_audited(
         with audit_mksession.begin() as audit_session:
             change_logs = audit_session.execute(select(ChangeLog)).scalars().all()
             assert len(change_logs) == 1
+            assert change_logs[0].type == "insert"
+            assert change_logs[0].executed_at <= now
+            assert change_logs[0].table_name == Person.__tablename__
+            assert change_logs[0].diff == expected_diff
