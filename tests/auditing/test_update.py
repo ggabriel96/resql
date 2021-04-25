a37fa2013ea35e87eea99dbaa7bba867d4ef17ba
Gabriel Galli <ggabriel96@hotmail.com>
2021-04-25 13:58:34 -0300

    test_orm_enabled_update_statement_is_not_audited

--- tests/auditing/test_update.py
+++ tests/auditing/test_update.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.future import Engine
 from sqlalchemy.orm import sessionmaker
 
@@ -49,3 +49,48 @@ def test_orm_update_should_be_audited(
         assert dt_before <= change_logs[0].executed_at <= dt_after
         assert change_logs[0].table_name == Person.__tablename__
         assert change_logs[0].diff == expected_diff
+
+
+def test_orm_enabled_update_statement_is_not_audited(
+    audit_engine: Engine, audit_mksession: sessionmaker, production_mksession: sessionmaker
+) -> None:
+    """
+    See the warning in https://docs.sqlalchemy.org/en/14/orm/session_basics.html#selecting-a-synchronization-strategy:
+
+    In order to intercept ORM-enabled UPDATE and DELETE operations with event handlers,
+    use the SessionEvents.do_orm_execute() event.
+    """
+    # Arrange
+    people = [
+        Person(name="Update This 1", age=1),
+        Person(name="This Is Correct", age=2),
+        Person(name="Update This 2", age=3),
+    ]
+    with production_mksession.begin() as session:  # type: ignore[no-untyped-call]
+        session.add_all(people)
+
+    # Act
+    log_changes(of=production_mksession, to=audit_engine)
+    with production_mksession.begin() as session:  # type: ignore[no-untyped-call]
+        update_result = session.execute(
+            update(Person)
+            .where(Person.name.contains("Update This"))
+            .values(name="This Is Correct")
+            .execution_options(synchronize_session="fetch")
+        )
+        assert update_result.rowcount == 2
+
+    # Assert
+    with production_mksession.begin() as session:  # type: ignore[no-untyped-call]
+        inserted_people = session.execute(select(Person).order_by(Person.age)).scalars().all()
+        assert len(inserted_people) == 3
+        assert inserted_people[0].name == "This Is Correct"
+        assert inserted_people[1].name == "This Is Correct"
+        assert inserted_people[2].name == "This Is Correct"
+        assert inserted_people[0].age == 1
+        assert inserted_people[1].age == 2
+        assert inserted_people[2].age == 3
+
+    with audit_mksession.begin() as audit_session:  # type: ignore[no-untyped-call]
+        change_logs = audit_session.execute(select(ChangeLog)).scalars().all()
+        assert len(change_logs) == 0
