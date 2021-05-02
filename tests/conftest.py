238bb2d0a4f22e0c1fb5981254132ba69b5a0224
Gabriel Galli <ggabriel96@hotmail.com>
2021-05-02 02:53:48 -0300

    truncate_all in engine fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,8 +17,8 @@ def _env() -> Environment:
     return Environment()
 
 
-@fixture(name="audit_engine", scope="session")
-def _audit_engine(env: Environment) -> Engine:
+@fixture(name="audit_engine", scope="function")
+def _audit_engine(env: Environment) -> Iterator[Engine]:
     audit_engine = create_engine(
         env.audit_url,
         echo=True,
@@ -26,18 +26,19 @@ def _audit_engine(env: Environment) -> Engine:
         logging_name="AUDITING",
     )
     AuditingBase.metadata.create_all(audit_engine)
-    return audit_engine  # type: ignore[return-value]
+    yield audit_engine  # type: ignore[misc]
+    truncate_all(audit_engine)  # type: ignore[arg-type]
 
 
-@fixture(name="audit_now", scope="session")
+@fixture(name="audit_now", scope="function")
 def _audit_now(audit_engine: Engine) -> dt.datetime:
     if "postgresql" in audit_engine.dialect.dialect_description:
         return dt.datetime.now(dt.timezone.utc)
     return dt.datetime.utcnow().replace(microsecond=0)
 
 
-@fixture(name="recovery_engine", scope="session")
-def _recovery_engine(env: Environment) -> Engine:
+@fixture(name="recovery_engine", scope="function")
+def _recovery_engine(env: Environment) -> Iterator[Engine]:
     recovery_engine = create_engine(
         env.recovery_url,
         echo=True,
@@ -45,11 +46,19 @@ def _recovery_engine(env: Environment) -> Engine:
         logging_name="RECOVERY",
     )
     RecoveryBase.metadata.create_all(recovery_engine)
-    return recovery_engine  # type: ignore[return-value]
+    yield recovery_engine  # type: ignore[misc]
+    truncate_all(recovery_engine)  # type: ignore[arg-type]
 
 
-@fixture(name="production_engine", scope="session")
-def _production_engine(env: Environment) -> Engine:
+@fixture(name="recovery_now", scope="function")
+def _recovery_now(recovery_engine: Engine) -> dt.datetime:
+    if "postgresql" in recovery_engine.dialect.dialect_description:
+        return dt.datetime.now(dt.timezone.utc)
+    return dt.datetime.utcnow().replace(microsecond=0)
+
+
+@fixture(name="production_engine", scope="function")
+def _production_engine(env: Environment) -> Iterator[Engine]:
     production_engine = create_engine(
         env.production_url,
         echo=True,
@@ -57,22 +66,20 @@ def _production_engine(env: Environment) -> Engine:
         logging_name="PRODUCTN",
     )
     ProductionBase.metadata.create_all(production_engine)
-    return production_engine  # type: ignore[return-value]
+    yield production_engine  # type: ignore[misc]
+    truncate_all(production_engine)  # type: ignore[arg-type]
 
 
 @fixture(name="audit_mksession", scope="function")
-def _audit_mksession(audit_engine: Engine) -> Iterator[sessionmaker]:
-    yield sessionmaker(audit_engine, future=True)
-    truncate_all(audit_engine)
+def _audit_mksession(audit_engine: Engine) -> sessionmaker:
+    return sessionmaker(audit_engine, future=True)
 
 
 @fixture(name="recovery_mksession", scope="function")
-def _recovery_mksession(recovery_engine: Engine) -> Iterator[sessionmaker]:
-    yield sessionmaker(recovery_engine, future=True)
-    truncate_all(recovery_engine)
+def _recovery_mksession(recovery_engine: Engine) -> sessionmaker:
+    return sessionmaker(recovery_engine, future=True)
 
 
 @fixture(name="production_mksession", scope="function")
-def _production_mksession(production_engine: Engine) -> Iterator[sessionmaker]:
-    yield sessionmaker(production_engine, future=True)
-    truncate_all(production_engine)
+def _production_mksession(production_engine: Engine) -> sessionmaker:
+    return sessionmaker(production_engine, future=True)
