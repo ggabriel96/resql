2cc6fa60b8aea8dd721696bf0f6df5cb97823275
Gabriel Galli <ggabriel96@hotmail.com>
2021-10-23 10:46:27 -0300

    test: add and use custom json serializers for Engine instances

--- tests/database.py
+++ tests/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
 from tests.settings import Environment
+from tests.utils import from_json, to_json
 
 AUDIT_ENGINE: Engine
 RECOVERY_ENGINE: Engine
@@ -10,6 +11,27 @@ PRODUCTION_ENGINE: Engine
 
 def init_engines(env: Environment) -> None:
     global AUDIT_ENGINE, RECOVERY_ENGINE, PRODUCTION_ENGINE  # pylint: disable=global-statement
-    AUDIT_ENGINE = create_engine(env.audit_url, echo=True, future=True, logging_name="AUDITING")
-    RECOVERY_ENGINE = create_engine(env.recovery_url, echo=True, future=True, logging_name="RECOVERY")
-    PRODUCTION_ENGINE = create_engine(env.production_url, echo=True, future=True, logging_name="PRODUCTN")
+    AUDIT_ENGINE = create_engine(
+        env.audit_url,
+        echo=True,
+        future=True,
+        json_deserializer=from_json,
+        json_serializer=to_json,
+        logging_name="AUDITING",
+    )
+    RECOVERY_ENGINE = create_engine(
+        env.recovery_url,
+        echo=True,
+        future=True,
+        json_deserializer=from_json,
+        json_serializer=to_json,
+        logging_name="RECOVERY",
+    )
+    PRODUCTION_ENGINE = create_engine(
+        env.production_url,
+        echo=True,
+        future=True,
+        json_deserializer=from_json,
+        json_serializer=to_json,
+        logging_name="PRODUCTN",
+    )
--- tests/utils.py
+++ tests/utils.py
@@ -1,13 +1,23 @@
 import datetime as dt
 from dataclasses import dataclass
+from typing import Any, cast
 
+import rapidjson
 from sqlalchemy import MetaData
 from sqlalchemy.future import Engine
 from sqlalchemy.orm import registry
 
 
+def from_json(obj: str) -> Any:
+    return rapidjson.loads(obj, datetime_mode=rapidjson.DM_ISO8601, uuid_mode=rapidjson.UM_CANONICAL)
+
+
 def now_in_utc() -> dt.datetime:
     return dt.datetime.now(tz=dt.timezone.utc)
+
+
+def to_json(obj: Any) -> str:
+    return cast(str, rapidjson.dumps(obj, datetime_mode=rapidjson.DM_ISO8601, uuid_mode=rapidjson.UM_CANONICAL))
 
 
 def truncate_all(engine: Engine) -> None:
