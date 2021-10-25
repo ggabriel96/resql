0d67e81e752b7c0671bd84ac52a01627e4f1e9c3
Gabriel Galli <ggabriel96@hotmail.com>
2021-10-24 22:05:42 -0300

    test: migrate more tests to use freezegun

--- tests/query_log/test_insert.py
+++ tests/query_log/test_insert.py
@@ -1,6 +1,6 @@
 import copy
-import datetime as dt
 
+from freezegun import freeze_time
 from sqlalchemy import insert, select
 from sqlalchemy.future import Engine
 from sqlalchemy.orm import sessionmaker
@@ -19,19 +19,18 @@ def test_extra_field_is_reused_across_commits_on_same_engine(
 ) -> None:
     # Arrange
     now = now_in_utc()
-    dt_before = now - dt.timedelta(seconds=1)
-    dt_after = now + dt.timedelta(seconds=1)
     extra = dict(user_agent="testing")
     person_1 = dict(name="A", age=1)
     person_2 = dict(name="B", age=2)
 
     # Act
     with production_engine.connect() as conn:
-        log_queries(of=conn, to=recovery_engine, extra=copy.deepcopy(extra))
-        conn.execute(insert(Person).values(**person_1))
-        conn.commit()
-        conn.execute(insert(Person).values(**person_2))
-        conn.commit()
+        with freeze_time(now):
+            log_queries(of=conn, to=recovery_engine, extra=copy.deepcopy(extra))
+            conn.execute(insert(Person).values(**person_1))
+            conn.commit()
+            conn.execute(insert(Person).values(**person_2))
+            conn.commit()
 
     # Assert
     with production_engine.connect() as conn:
@@ -43,8 +42,8 @@ def test_extra_field_is_reused_across_commits_on_same_engine(
         assert len(query_logs) == 2
         assert query_logs[0].dialect_description == getattr(production_engine.dialect, "dialect_description", ...)
         assert query_logs[1].dialect_description == getattr(production_engine.dialect, "dialect_description", ...)
-        assert dt_before <= query_logs[0].executed_at <= dt_after
-        assert dt_before <= query_logs[1].executed_at <= dt_after
+        assert query_logs[0].executed_at == now
+        assert query_logs[1].executed_at == now
         assert query_logs[0].extra == extra
         assert query_logs[1].extra == extra
         assert query_logs[0].type == OpType.INSERT
@@ -58,22 +57,21 @@ def test_extra_field_is_saved_independently_for_concurrent_connections(
 ) -> None:
     # Arrange
     now = now_in_utc()
-    dt_before = now - dt.timedelta(seconds=1)
-    dt_after = now + dt.timedelta(seconds=1)
     extra_1 = dict(session_no=1)
     extra_2 = dict(session_no=2)
     people = [dict(name="A", age=1), dict(name="B", age=2)]
 
     # Act
-    with production_engine.connect() as conn_1:
-        log_queries(of=conn_1, to=recovery_engine, extra=copy.deepcopy(extra_1))
-        with production_engine.connect() as conn_2:
-            log_queries(of=conn_2, to=recovery_engine, extra=copy.deepcopy(extra_2))
+    with freeze_time(now):
+        with production_engine.connect() as conn_1:
+            log_queries(of=conn_1, to=recovery_engine, extra=copy.deepcopy(extra_1))
+            with production_engine.connect() as conn_2:
+                log_queries(of=conn_2, to=recovery_engine, extra=copy.deepcopy(extra_2))
 
-            conn_1.execute(insert(Person).values(**people[0]))
-            conn_1.commit()
-            conn_2.execute(insert(Person).values(**people[1]))
-            conn_2.commit()
+                conn_1.execute(insert(Person).values(**people[0]))
+                conn_1.commit()
+                conn_2.execute(insert(Person).values(**people[1]))
+                conn_2.commit()
 
     # Assert
     with production_engine.connect() as conn:
@@ -85,8 +83,8 @@ def test_extra_field_is_saved_independently_for_concurrent_connections(
         assert len(query_logs) == 2
         assert query_logs[0].dialect_description == getattr(production_engine.dialect, "dialect_description", ...)
         assert query_logs[1].dialect_description == getattr(production_engine.dialect, "dialect_description", ...)
-        assert dt_before <= query_logs[0].executed_at <= dt_after
-        assert dt_before <= query_logs[1].executed_at <= dt_after
+        assert query_logs[0].executed_at == now
+        assert query_logs[1].executed_at == now
         assert query_logs[0].extra == extra_1
         assert query_logs[1].extra == extra_2
         assert query_logs[0].type == OpType.INSERT
